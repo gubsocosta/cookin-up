79897074d258485d1e343221c83217604320be62
Gabriel Costa <gubsocosta@gmail.com>
2025-04-04 15:56:11 -0300

    feat: create http service

---
+++ src/interfaces/ICategory.ts
@@ -0,0 +1,11 @@
+export interface ICategory {
+  name: string;
+  ingredients: string[];
+  label: string;
+}
+
+export interface ICategoryResponse {
+  nome: string;
+  ingredientes: string[];
+  rotulo: string;
+}
---
+++ src/services/http.ts
@@ -0,0 +1,12 @@
+import type { ICategory, ICategoryResponse } from '@/interfaces/ICategory.ts';
+
+export async function getCategories (): Promise<ICategory[]> {
+  const response = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+  const categories: ICategoryResponse[] = await response.json();
+
+  return categories.map(category => ({
+    name: category.nome,
+    ingredients: category.ingredientes,
+    label: category.rotulo
+  }));
+}
