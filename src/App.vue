2a241fb6419f9bfb9ea5c632b4701b8443655943
Gabriel Costa <gubsocosta@gmail.com>
2025-04-04 15:02:08 -0300

    feat: create main content component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Banner />
+  <MainContent />
 </template>
 
 <script setup lang="ts">
 import Banner from './components/Banner.vue';
+import MainContent from './components/MainContent.vue';
 </script>
 
 <style scoped>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,85 @@
+<template>
+  <main class="conteudo-principal">
+    <section>
+      <span class="subtitulo-lg sua-lista-texto">Sua Lista</span>
+      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+        <li class="ingrediente" v-for="(item, index) in ingredientes" :key="index">{{item}}</li>
+      </ul>
+      <p v-else class="paragrafo lista-vazia">
+        <img src="../assets/images/icones/lista-vazia.svg" alt="Icone de pesquisa">
+        Sua lista está vazia, selecione ingredientes para iniciar.
+      </p>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const ingredientes = ref(['alho', 'oregano', 'manteiga']);
+</script>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.ingrediente {
+  display: inline-block;
+  border-radius: 0.5rem;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+  transition: 0.2s;
+  color: var(--creme, #FFFAF3);
+  background: var(--coral, #F0633C);
+  font-weight: 700;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
