018d8e4cf5ec5cfc9d913865de156b7eef7abf6e
Gabriel Costa <gubsocosta@gmail.com>
2025-04-06 18:44:16 -0300

    refactor: optmize structure

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,10 +1,14 @@
 import type { ICategory, ICategoryResponse } from '@/interfaces/ICategory.ts';
 import type { IRecipe, IRecipeResponse } from '@/interfaces/IRecipe.ts';
 
+async function getData<T> (url: string): Promise<T> {
+  const response = await fetch(url);
+  return await response.json() as T;
+}
+
 export async function getCategories (): Promise<ICategory[]> {
   try {
-    const response = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
-    const categories: ICategoryResponse[] = await response.json();
+    const categories= await getData<ICategoryResponse[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
 
     return categories.map((category): ICategory => ({
       name: category.nome,
@@ -19,9 +23,9 @@ export async function getCategories (): Promise<ICategory[]> {
 
 export async function getRecipes (): Promise<IRecipe[]> {
   try {
-    const response = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
-    const recipes: IRecipeResponse[] = await response.json();
+    const recipes = await getData<IRecipeResponse[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
 
+    await new Promise(resolve => setTimeout(resolve, 2000));
     return recipes.map((recipe): IRecipe => ({
       name: recipe.nome,
       ingredients: recipe.ingredientes,
